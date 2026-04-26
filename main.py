c996a39e636678e0b4ef1f27f601754c30c6e2a5
Yamid Claro <yclaro093@gmail.com>
2026-04-26 15:25:36 -0500

    Se crea archivo main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# ------------------------------------------------------------
+# Solución del Sistema de gestión de Clientes Servivios y Reservas
+# Grupo: 213023_145
+# Programa: Ingeniería de Sistemas
+# Código fuente: Autoría propia
+#
+# INTEGRANTES
+# Yamid Claro Alvarez
+# ------------------------------------------------------------
+
+# INICIO
